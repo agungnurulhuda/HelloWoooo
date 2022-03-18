49aae013b65094cc082bb0459de02238a3fdeda1
416Agung <90314106+agungnurulhuda@users.noreply.github.com>
2022-03-18 07:13:06 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<stdio.h>
+using namespace std;
+
+// Guys try guess this!
+#ifndef HEADER
+#define HEADER
+
+int main() {
+throw std::excep
+  
+  cout << "Hello Gais!\n";
+  cout <<"KRISNA CANTI, mwaahhh : *\n" ;
+  cout << "\n ahahahaha \n";
+  cout <<"Hujan deres bwangg\n";
+  cout <<"ayang banget ama kamu " << endl;betul udah kaya bogor jogja\n";
+  
+  cout<<"\nApa nih!!!\n";
+  cout<< "\nEmotional Damage!!\n";
+  cout<< "COKLATTTT"\n";
+
+
+    cout << "hows your day?\n";
+    
+    }
+
+#endif
+
